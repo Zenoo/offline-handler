53ae03d78be54872a6320551505a7744065c7e16
Zenoo <fzenooo@gmail.com>
2018-12-11 14:03:13 +0100

    Cleanup

--- OfflineHandler.js
+++ OfflineHandler.js
@@ -31,9 +31,6 @@ class OfflineHandler{
 				clearOldCache: typeof clearOldCache == 'undefined' || clearOldCache
 			};
 
-			console.log(this._parameters);
-			console.log(JSON.stringify(this._parameters));
-
 			if('serviceWorker' in navigator){
 				navigator.serviceWorker
 					.register(this._parameters.serviceWorkerPath + '?param=' + encodeURIComponent(JSON.stringify(this._parameters)));
